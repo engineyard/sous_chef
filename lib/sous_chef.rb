6204cd69fcadf2c027be8cdc87623a81159bd534
Ultra Ninja <ninja@engineyard.com>
2010-05-11 10:31:49 -0700

    Autoload must die.
    * autoload is not thread safe. we use threads. ergo, we should not use autoload.

--- lib/sous_chef.rb
+++ lib/sous_chef.rb
@@ -1,9 +1,9 @@
 module SousChef
-  autoload :Recipe,   'sous_chef/recipe'
-  autoload :Resource, 'sous_chef/resource'
-
   def self.prep(*flags, &block)
     recipe = Recipe.new(*flags, &block)
     recipe.to_script
   end
 end
+
+require 'sous_chef/recipe'
+require 'sous_chef/resource'
--- lib/sous_chef/resource.rb
+++ lib/sous_chef/resource.rb
@@ -1,10 +1,11 @@
 module SousChef
   module Resource
-    autoload :Base,      'sous_chef/resource/base'
-    autoload :Directory, 'sous_chef/resource/directory'
-    autoload :Execute,   'sous_chef/resource/execute'
-    autoload :File,      'sous_chef/resource/file'
-    autoload :Gemfile,   'sous_chef/resource/gemfile'
-    autoload :Log,       'sous_chef/resource/log'
   end
 end
+
+require 'sous_chef/resource/base'
+require 'sous_chef/resource/directory'
+require 'sous_chef/resource/execute'
+require 'sous_chef/resource/file'
+require 'sous_chef/resource/gemfile'
+require 'sous_chef/resource/log'
